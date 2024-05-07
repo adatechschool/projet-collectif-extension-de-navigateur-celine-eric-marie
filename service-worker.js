45111c8115257b73f70478ea7f5812b98d4d5f4a
Céline <celinesou77@gmail.com>
2024-05-07 10:25:56 +0200

    récupération du tableau de langues, rajout de toutes les langues dans le menu

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,11 @@
 let currentSelection = ""; //Variable globale pour stocker le texte selectionné dans la page web
-const allLanguages = [
-   {
-      code: "fr",
-      title: "Français",
-      menuItemId:null
-   },
-   {
-      code: "en",
-      title:"Anglais",
-      menuItemId: null
-   }
-]
+let allLanguages = [];
+let allNames = []
 
 //fonction pour traduire une string
 async function translateSelectedText(text, source = "auto", target = "fr") {
    try {
+      console.log(allNames)
       const res = await fetch("http://localhost:5000/translate", {
          method: "POST",
          body: JSON.stringify({
@@ -51,28 +42,25 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 
 /******************************************************************************************************************************/
 
-
 // Fonction pour créer ou mettre à jour l'option de menu contextuel
 function createOrUpdateContextMenu() {
    allLanguages.forEach((language) => {
       if (language.menuItemId === null) {
          // Créer l'option de menu contextuel pour l'onglet actif
-        language.menuItemId = "translateOption_"+ language.code;
+         language.menuItemId = "translateOption_" + language.code;
          chrome.contextMenus.create({
             id: language.menuItemId,
-            title: language.title,
+            title: language.name,
             contexts: ["selection"],
          });
       } else {
          // Mettre à jour l'option de menu contextuel pour l'onglet actif
          chrome.contextMenus.update(language.menuItemId, {
-            title: language.title,
+            title: language.name,
             contexts: ["selection"],
          });
       }
- }) 
-     
-   
+   });
 }
 
 // Écouter les changements d'onglet
@@ -81,22 +69,34 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
    createOrUpdateContextMenu();
 });
 
+chrome.runtime.onInstalled.addListener(async function () {
+   const allLanguagesData = await fetch("http://localhost:5000/languages");
+   allLanguages = await allLanguagesData.json();
+   allLanguages.forEach(oneLanguage => oneLanguage.menuItemId = null);
+   allNames = allLanguages.map(oneLanguage => ({[oneLanguage.code]: oneLanguage.name}));
+});
 
 /*******************************************************************************************************************************************/
 
 //fonction qui se déclenche dès qu'on clique sur l'option dans le menu
 chrome.contextMenus.onClicked.addListener(async function (info, tab) {
-   const targetLanguage = allLanguages.find((language) => info.menuItemId === language.menuItemId)
+   const targetLanguage = allLanguages.find(
+      (language) => info.menuItemId === language.menuItemId
+   );
    if (targetLanguage) {
-      const targetCode = targetLanguage.code
-      
-      const translatedText = await translateSelectedText(currentSelection,"auto",targetCode); //traduit le texte
+      const targetCode = targetLanguage.code;
+
+      const translatedText = await translateSelectedText(
+         currentSelection,
+         "auto",
+         targetCode
+      ); //traduit le texte
       console.log("3 Traduction : ", translatedText);
 
       //on envoie le texte traduit au script content
       chrome.tabs.sendMessage(tab.id, {
          type: "translation",
-         translatedText: translatedText
+         translatedText: translatedText,
       });
    }
 });
