95a7051c1157b24601ec6e7c9f8d706c7b718351
kevinleiba <k.leiba@free.fr>
2024-05-10 15:49:58 +0200

    code review 10/05/2024

--- content.js
+++ content.js
@@ -1,61 +1,67 @@
 // Fonction pour envoyer le texte sélectionné au script d'arrière-plan
 function sendSelectedTextToBackground(selectedText) {
-   console.log("1 Text sélectionné : ", selectedText);
-   chrome.runtime.sendMessage({
-      type: "selectedText",
-      text: selectedText,
-   });
+  console.log("1 Text sélectionné : ", selectedText);
+  chrome.runtime.sendMessage({
+    type: "selectedText",
+    text: selectedText,
+  });
 }
-
 
 // Écouteur pour capturer le texte sélectionné
 document.body.addEventListener("mouseup", () => {
-   const selectedText = window.getSelection().toString();
-   sendSelectedTextToBackground(selectedText);
+  const selectedText = window.getSelection().toString();
+  sendSelectedTextToBackground(selectedText);
 });
+
+function createTranslationModal(translationDialog, translationText) {
+  translationDialog = document.createElement("dialog");
+  translationText = document.createElement("p");
+  translationText.classList.add("paragraphe");
+  translationDialog.setAttribute("id", "translation-dialog");
+  translationText.setAttribute("id", "translation-text");
+  const closeForm = document.createElement("form");
+  const closeButton = document.createElement("button");
+  closeButton.classList.add("buttonClose");
+  closeButton.append("FERMER");
+
+  closeForm.setAttribute("method", "dialog");
+  closeForm.append(closeButton);
+
+  translationDialog.append(translationText, closeForm);
+  document.body.append(translationDialog);
+}
+
+function setTranslation(translation) {
+  let translationDialog = document.getElementById("translation-dialog");
+  let translationText = document.getElementById("translation-text");
+
+  translationText.textContent = "";
+  //on ajoute chaque paragraphe en transformant les "\n" en <br>
+  const translationArray = translation.split("\n");
+  translationArray.forEach((translationPiece) => {
+    const linebreak = document.createElement("br");
+    translationText.append(translationPiece);
+    translationText.append(linebreak);
+  });
+  translationDialog.setAttribute("open", "");
+}
 
 //Fonction pour afficher le texte dans une balise <dialog>
 function displayTranslationModal(translation) {
-   let translationDialog = document.getElementById("translation-dialog");
-   let translationText = document.getElementById("translation-text");
+  let translationDialog = document.getElementById("translation-dialog");
+  let translationText = document.getElementById("translation-text");
 
-   if (!translationDialog) {
-      translationDialog = document.createElement("dialog");
-      translationText = document.createElement("p");
-      translationText.classList.add("paragraphe")
-      translationDialog.setAttribute("id", "translation-dialog");
-      translationText.setAttribute("id", "translation-text");
-      const closeForm = document.createElement("form")
-      const closeButton = document.createElement("button")
-      closeButton.classList.add("buttonClose")
-      closeButton.append("FERMER")
+  if (!translationDialog)
+    createTranslationModal(translationDialog, translationText);
 
-      closeForm.setAttribute("method", "dialog")
-      closeForm.append(closeButton)
-
-
-      translationDialog.append(translationText, closeForm);
-      document.body.append(translationDialog);
-      displayTranslationModal(translation);
-   } else {
-      translationText.textContent = "";
-      //on ajoute chaque paragraphe en transformant les "\n" en <br>
-      const translationArray = translation.split("\n");
-      translationArray.forEach((translationPiece) => {
-         const linebreak = document.createElement("br");
-         translationText.append(translationPiece);
-         translationText.append(linebreak);
-      });
-      translationDialog.setAttribute("open", "");
-   }
+  setTranslation(translation);
 }
 
 //Ecouteur de message qui vient du service worker
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-   if (message.type === "translation") {
-      //on reçoit le texte traduit
-      console.log("4 Texte traduit reçu : ", message.translatedText);
-      displayTranslationModal(message.translatedText);
-   }
-})
-
+  if (message.type === "translation") {
+    //on reçoit le texte traduit
+    console.log("4 Texte traduit reçu : ", message.translatedText);
+    displayTranslationModal(message.translatedText);
+  }
+});
--- service-worker.js
+++ service-worker.js
@@ -1,110 +1,107 @@
 let currentSelection = ""; //Variable globale pour stocker le texte selectionné dans la page web
 let allLanguages = [];
-let allNames = [];
 
 //fonction pour traduire une string
-async function translateSelectedText(text, source = "auto", target = "fr") {
-   try {
-      const res = await fetch("http://localhost:5000/translate", {
-         method: "POST",
-         body: JSON.stringify({
-            q: text,
-            source: source,
-            target: target,
-            format: "text",
-            api_key: "",
-         }),
-         headers: { "Content-Type": "application/json" },
-      });
+async function getTranslatedSelectedText(text, source = "auto", target = "fr") {
+  try {
+    const res = await fetch("http://localhost:5000/translate", {
+      method: "POST",
+      body: JSON.stringify({
+        q: text,
+        source,
+        target,
+        format: "text",
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-      if (!res.ok) {
-         throw new Error("La réponse du réseau n'a pas été correcte");
-      }
+    if (!res.ok) {
+      throw new Error("La réponse du réseau n'a pas été correcte");
+    }
 
-      const data = await res.json();
-      const translatedText = data.translatedText;
-      return translatedText;
-   } catch (error) {
-      console.error("Une erreur est survenue:", error.message);
-   }
+    const data = await res.json();
+    return data.translatedText;
+  } catch (error) {
+    console.error("Une erreur est survenue:", error.message);
+  }
 }
 
 //Ecouter le message envoyé par content
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-   //Si on reçoit le texte selectionné dans la page web
-   if (message.type === "selectedText") {
-      //on stocke le texte dans la variable currentSelection
-      currentSelection = message.text;
-      console.log("2 selection reçu : ", currentSelection);
-   }
+  //Si on reçoit le texte selectionné dans la page web
+  if (message.type === "selectedText") {
+    //on stocke le texte dans la variable currentSelection
+    currentSelection = message.text;
+    console.log("2 selection reçu : ", currentSelection);
+  }
 });
 
 /******************************************************************************************************************************/
 
 // Fonction pour créer ou mettre à jour l'option de menu contextuel
 function createOrUpdateContextMenu() {
-   allLanguages.forEach((language) => {
-      if (language.menuItemId === null) {
-         // Créer l'option de menu contextuel pour l'onglet actif
-         language.menuItemId = "translateOption_" + language.code;
-         chrome.contextMenus.create({
-            id: language.menuItemId,
-            title: language.name,
-            contexts: ["selection"],
-         });
-      } else {
-         // Mettre à jour l'option de menu contextuel pour l'onglet actif
-         chrome.contextMenus.update(language.menuItemId, {
-            title: language.name,
-            contexts: ["selection"],
-         });
-      }
-   });
+  allLanguages.forEach((language) => {
+    if (language.menuItemId === null) {
+      // Créer l'option de menu contextuel pour l'onglet actif
+      language.menuItemId = "translateOption_" + language.code;
+      chrome.contextMenus.create({
+        id: language.menuItemId,
+        title: language.name,
+        contexts: ["selection"],
+      });
+    } else {
+      // Mettre à jour l'option de menu contextuel pour l'onglet actif
+      chrome.contextMenus.update(language.menuItemId, {
+        title: language.name,
+        contexts: ["selection"],
+      });
+    }
+  });
 }
 
 // Écouter les changements d'onglet
 chrome.tabs.onActivated.addListener(function (activeInfo) {
-   // Mettre à jour l'option de menu contextuel pour l'onglet actif
-   createOrUpdateContextMenu();
+  // Mettre à jour l'option de menu contextuel pour l'onglet actif
+  createOrUpdateContextMenu();
 });
 
 chrome.runtime.onStartup.addListener(async function () {
-   const allLanguagesData = await fetch("http://localhost:5000/languages");
-   allLanguages = await allLanguagesData.json();
-   allLanguages.forEach((oneLanguage) => (oneLanguage.menuItemId = null));
-   allLanguages.sort((a, b) => a.name.localeCompare(b.name));
-   allNames = allLanguages.map((oneLanguage) => ({
-      [oneLanguage.code]: oneLanguage.name,
-   }));
-   console.log("allLanguages = ", allLanguages);
-   chrome.contextMenus.removeAll(function () {
-      createOrUpdateContextMenu();
-   });
+  const allLanguagesData = await fetch("http://localhost:5000/languages");
+  const rawAllLanguagesData = await allLanguagesData.json();
+  allLanguages = rawAllLanguagesData.map((language) => ({
+    ...language,
+    menuItemId: null,
+  }));
+
+  allLanguages.sort((a, b) => a.name.localeCompare(b.name));
+
+  console.log("allLanguages = ", allLanguages);
+  chrome.contextMenus.removeAll(function () {
+    createOrUpdateContextMenu();
+  });
 });
 
 /*******************************************************************************************************************************************/
 
 //fonction qui se déclenche dès qu'on clique sur l'option dans le menu
 chrome.contextMenus.onClicked.addListener(async function (info, tab) {
-   console.log("menu cliqué, info.menuItemId =", info.menuItemId);
-   const targetLanguage = allLanguages.find(
-      (language) => info.menuItemId === language.menuItemId
-   );
-   console.log("langage :", targetLanguage);
-   if (targetLanguage) {
-      const targetCode = targetLanguage.code;
+  console.log("menu cliqué, info.menuItemId =", info.menuItemId);
+  const targetLanguage = allLanguages.find(
+    (language) => info.menuItemId === language.menuItemId
+  );
+  console.log("langage :", targetLanguage);
+  if (targetLanguage) {
+    const translatedText = await getTranslatedSelectedText(
+      currentSelection,
+      "auto",
+      targetLanguage.code
+    ); //traduit le texte
+    console.log("3 Traduction : ", translatedText);
 
-      const translatedText = await translateSelectedText(
-         currentSelection,
-         "auto",
-         targetCode
-      ); //traduit le texte
-      console.log("3 Traduction : ", translatedText);
-
-      //on envoie le texte traduit au script content
-      chrome.tabs.sendMessage(tab.id, {
-         type: "translation",
-         translatedText: translatedText,
-      });
-   }
+    //on envoie le texte traduit au script content
+    chrome.tabs.sendMessage(tab.id, {
+      type: "translation",
+      translatedText: translatedText,
+    });
+  }
 });
